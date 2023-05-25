f1f07efaee50a4fbc91b7d82edbff0c1439a2dd5
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-05-25 11:06:07 +0200

    Update settings.py

--- scanner/settings.py
+++ scanner/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-igpv5)7#j5d1t6y(n^i=9!(*vi*mde!-7@!irh=9+6ik)qblp9'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nessus'
+    'nessus',
 ]
 
 MIDDLEWARE = [
