64dd7d8061be6b0be4e6329912077d3f5e0d0ce2
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-06-06 11:25:50 +0200

    Update urls.py

--- nessus/urls.py
+++ nessus/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('domains', views.AttackSurfaceDomains,name="domains"),
     path('domains/insert', views.InsertOKDomain,name="domains_insert"),
+    path('domains/urlscan', views.DomainURLScan,name="domains_urlscan"),
 ]
