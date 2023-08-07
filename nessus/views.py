e1338158614d97f577d2b20e9de66a9a32070235
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-08-07 14:56:12 +0200

    Update views.py

--- nessus/views.py
+++ nessus/views.py
@@ -13,258 +13,258 @@ import socket
 from datetime import datetime
 
 def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+	context = {}
+	return render(request, 'index.html', context)
 
 
 def UpdateDomainComments(request, pk):
-    okdomain = get_object_or_404(OKDomains, pk=pk)
-    if request.method == 'POST':
-        new_comments = request.POST.get('comments')
-        okdomain.comments = new_comments
-        okdomain.save()
-    okdomains = OKDomains.objects.all()
-    return render(request, 'domains.html', {'okdomains': okdomains})
+	okdomain = get_object_or_404(OKDomains, pk=pk)
+	if request.method == 'POST':
+		new_comments = request.POST.get('comments')
+		okdomain.comments = new_comments
+		okdomain.save()
+	okdomains = OKDomains.objects.all()
+	return render(request, 'domains.html', {'okdomains': okdomains})
 
 # Example usage:
 def AttackSurfaceDomains(request):
-    okdomains = OKDomains.objects.all()
-    for domain_entry in okdomains:
-        if domain_entry.urlscan:
-            # Load the urlscan data into JSON.
-            domain_entry.urlscan = json.loads(domain_entry.urlscan)
+	okdomains = OKDomains.objects.all()
+	for domain_entry in okdomains:
+		if domain_entry.urlscan:
+			# Load the urlscan data into JSON.
+			domain_entry.urlscan = json.loads(domain_entry.urlscan)
 
-            # Modify the time variable to only display the day of the scan.
-            domain_entry.urlscan["task"]["time"] = domain_entry.urlscan["task"]["time"].split("T")[0]
+			# Modify the time variable to only display the day of the scan.
+			domain_entry.urlscan["task"]["time"] = domain_entry.urlscan["task"]["time"].split("T")[0]
 
-        if domain_entry.nmap:
-            domain_entry.nmap = FilterNMAP(domain_entry.nmap)
-        if domain_entry.ip_data:
-            domain_entry.ip_data = json.loads(domain_entry.ip_data)
-    return render(request, 'domains.html', {'okdomains': okdomains})
+		if domain_entry.nmap:
+			domain_entry.nmap = FilterNMAP(domain_entry.nmap)
+		if domain_entry.ip_data:
+			domain_entry.ip_data = json.loads(domain_entry.ip_data)
+	return render(request, 'domains.html', {'okdomains': okdomains})
 
 
 def DomainIPCheck(request):
-    all_domains = OKDomains.objects.values_list('domain')
-    for entry in all_domains:
-        # Use subprocess.Popen to run the ping command and capture its output
-        ping_process = subprocess.Popen(['ping', entry[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = ping_process.communicate()
+	all_domains = OKDomains.objects.values_list('domain')
+	for entry in all_domains:
+		# Use subprocess.Popen to run the ping command and capture its output
+		ping_process = subprocess.Popen(['ping', entry[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+		stdout, stderr = ping_process.communicate()
 
-        # Parse the output to extract the IP address
-        for line in stdout.splitlines():
-            if 'Pinging' in line:
-                ip_start = line.find('[') + 1
-                ip_end = line.find(']')
-                ip = line[ip_start:ip_end]
-                # Update database entry
-                existing_entry = OKDomains.objects.get(domain=entry[0])
-                existing_entry.ip = ip
-                existing_entry.save()
-    return AttackSurfaceDomains(request)
+		# Parse the output to extract the IP address
+		for line in stdout.splitlines():
+			if 'Pinging' in line:
+				ip_start = line.find('[') + 1
+				ip_end = line.find(']')
+				ip = line[ip_start:ip_end]
+				# Update database entry
+				existing_entry = OKDomains.objects.get(domain=entry[0])
+				existing_entry.ip = ip
+				existing_entry.save()
+	return AttackSurfaceDomains(request)
 
 
 def IPGeoLookup(request):
-    if request.method == 'POST':
-        # Fetches all IP addresses from the database.
-        all_ip_addresses = list(OKDomains.objects.values_list('ip', flat=True))
-        for ip_address in all_ip_addresses:
-            api_endpoint = "https://api.iplocation.net/?ip={}".format(ip_address)
-            # Fetch data from the API
-            response = requests.get(api_endpoint)
-            if response.status_code == 200:
-                api_data = response.json()
-                api_data_json = json.dumps(api_data)
-                print(api_data)
+	if request.method == 'POST':
+		# Fetches all IP addresses from the database.
+		all_ip_addresses = list(OKDomains.objects.values_list('ip', flat=True))
+		for ip_address in all_ip_addresses:
+			api_endpoint = "https://api.iplocation.net/?ip={}".format(ip_address)
+			# Fetch data from the API
+			response = requests.get(api_endpoint)
+			if response.status_code == 200:
+				api_data = response.json()
+				api_data_json = json.dumps(api_data)
+				print(api_data)
 
-                # Update database entries
-                existing_entries = OKDomains.objects.filter(ip=dict(api_data)["ip"])
+				# Update database entries
+				existing_entries = OKDomains.objects.filter(ip=dict(api_data)["ip"])
 
-                for existing_entry in existing_entries:
-                    existing_entry.ip_data = api_data_json
-                    existing_entry.save()
-                existing_entries = OKDomains.objects.filter(ip=dict(api_data)["ip"])
-            else:
-                print("Error")
+				for existing_entry in existing_entries:
+					existing_entry.ip_data = api_data_json
+					existing_entry.save()
+				existing_entries = OKDomains.objects.filter(ip=dict(api_data)["ip"])
+			else:
+				print("Error")
 
-    return AttackSurfaceDomains(request)
+	return AttackSurfaceDomains(request)
 
 
 def URLScan(request, url):
-    data = []
-    base_query = 'https://urlscan.io/api/v1/search/?q=domain:{}'.format(url)
-    header = {"Authorization": os.environ["URLSCAN_API_SECRET"]}
-    result = requests.get(base_query, headers=header).json()
-    try:
-        result_total = result["total"]
-        result_took = result["took"]
-        for entry in result["results"]:
-            data.append(entry)
-        return json.dumps(data)  # Return the JSON data instead of saving it directly
-    except:
-        print("[{}] Error: Could not parse URLScan result: {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), result))
+	data = []
+	base_query = 'https://urlscan.io/api/v1/search/?q=domain:{}'.format(url)
+	header = {"Authorization": os.environ["URLSCAN_API_SECRET"]}
+	result = requests.get(base_query, headers=header).json()
+	try:
+		result_total = result["total"]
+		result_took = result["took"]
+		for entry in result["results"]:
+			data.append(entry)
+		return json.dumps(data)  # Return the JSON data instead of saving it directly
+	except:
+		print("[{}] Error: Could not parse URLScan result: {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), result))
 
 def UpdateURLScanData(domain, data, existing_entry):
-    # Parse the JSON data into a list of dictionaries (assuming the data is a list)
-    data_list = json.loads(data)
-    if data_list:  # Check if the list is not empty
-        data_dict = data_list[0]  # Access the dictionary inside the list
-        existing_entry.urlscan = json.dumps(data_dict)  # Save the dictionary as a JSON string
-        existing_entry.url = data_dict["task"].get("domain")
-        existing_entry.tlsissuer = data_dict["page"].get("tlsIssuer")
-        existing_entry.screenshot = data_dict.get("screenshot")
-        existing_entry.server = data_dict["page"].get("server")
-        existing_entry.save()
+	# Parse the JSON data into a list of dictionaries (assuming the data is a list)
+	data_list = json.loads(data)
+	if data_list:  # Check if the list is not empty
+		data_dict = data_list[0]  # Access the dictionary inside the list
+		existing_entry.urlscan = json.dumps(data_dict)  # Save the dictionary as a JSON string
+		existing_entry.url = data_dict["task"].get("domain")
+		existing_entry.tlsissuer = data_dict["page"].get("tlsIssuer")
+		existing_entry.screenshot = data_dict.get("screenshot")
+		existing_entry.server = data_dict["page"].get("server")
+		existing_entry.save()
 
 def DomainURLScan(request):
-    all_domains = OKDomains.objects.values_list('domain')
-    counter = 0
-    for entry in all_domains:
-        domain = entry[0].replace('http://', '').replace('https://', '').replace('/', '').replace('www.', '')
-        existing_entry = OKDomains.objects.filter(domain=domain).first()
-        if existing_entry and not existing_entry.urlscan:
-          # Check if the urlscan field is empty
-            data = URLScan(request, domain)
-            if data:
-                UpdateURLScanData(domain, data, existing_entry)
-                print("Updating: {} | {}".format(domain, data))  # Update the data only if the urlscan field is empty
-                counter += 1
-                if counter >= 24:
-                    print("[URLScan] Limit reached, waiting 15 minutes before continuing. Last domain was: {}".format(domain))
-                    time.sleep(900)
-            else:
-                print("[URLScan] Could not get data for: {}".format(domain))
-        else:
-            print("Existing entry and data for: {}".format(domain))
-        
-    return AttackSurfaceDomains(request)
+	all_domains = OKDomains.objects.values_list('domain')
+	counter = 0
+	for entry in all_domains:
+		domain = entry[0].replace('http://', '').replace('https://', '').replace('/', '').replace('www.', '')
+		existing_entry = OKDomains.objects.filter(domain=domain).first()
+		if existing_entry and not existing_entry.urlscan:
+		  # Check if the urlscan field is empty
+			data = URLScan(request, domain)
+			if data:
+				UpdateURLScanData(domain, data, existing_entry)
+				print("Updating: {} | {}".format(domain, data))  # Update the data only if the urlscan field is empty
+				counter += 1
+				if counter >= 24:
+					print("[URLScan] Limit reached, waiting 15 minutes before continuing. Last domain was: {}".format(domain))
+					time.sleep(900)
+			else:
+				print("[URLScan] Could not get data for: {}".format(domain))
+		else:
+			print("Existing entry and data for: {}".format(domain))
+		
+	return AttackSurfaceDomains(request)
 
 
 def NMAPDomainScan(request):
-    if request.method == 'POST':
-        domains = request.POST.get('domains').split(",")
-        for domain_name in domains:
-            try:
-                # Run Nmap as an external command
-                nmap_args = ["nmap", "-T4", "-F", domain_name]
-                nmap_output = subprocess.run(nmap_args, capture_output=True, text=True)
+	if request.method == 'POST':
+		domains = request.POST.get('domains').split(",")
+		for domain_name in domains:
+			try:
+				# Run Nmap as an external command
+				nmap_args = ["nmap", "-T4", "-F", domain_name]
+				nmap_output = subprocess.run(nmap_args, capture_output=True, text=True)
 
-                # Extract the scan output from the completed process
-                nmap_data = nmap_output.stdout
+				# Extract the scan output from the completed process
+				nmap_data = nmap_output.stdout
 
-                # Create or update the record in the OKDomains table
-                ok_domain, created = OKDomains.objects.get_or_create(domain=domain_name)
-                ok_domain.nmap = nmap_data
-                ok_domain.save()
+				# Create or update the record in the OKDomains table
+				ok_domain, created = OKDomains.objects.get_or_create(domain=domain_name)
+				ok_domain.nmap = nmap_data
+				ok_domain.save()
 
-            except subprocess.CalledProcessError as e:
-                # Handle any errors that occurred while running Nmap
-                error_message = f"Error running Nmap for domain '{domain_name}': {e}"
-                print(error_message)
+			except subprocess.CalledProcessError as e:
+				# Handle any errors that occurred while running Nmap
+				error_message = f"Error running Nmap for domain '{domain_name}': {e}"
+				print(error_message)
 
-    return AttackSurfaceDomains(request)
+	return AttackSurfaceDomains(request)
 
 
 def FilterNMAP(nmap_results):
-    import re
-    open_ports = []
-    pattern = r"(\d+)/tcp\s+open"
+	import re
+	open_ports = []
+	pattern = r"(\d+)/tcp\s+open"
 
-    matches = re.findall(pattern, nmap_results)
-    for port in matches:
-        open_ports.append(int(port))
+	matches = re.findall(pattern, nmap_results)
+	for port in matches:
+		open_ports.append(int(port))
 
-    return open_ports
+	return open_ports
 
 
 def NessusScan(request):
-    api_key = os.environ["NESSUS_API_ACCESS_KEY"]
-    url = "https://nessus.okcsirt.no/api/v2/scans"
+	api_key = os.environ["NESSUS_API_ACCESS_KEY"]
+	url = "https://nessus.okcsirt.no/api/v2/scans"
 
-    headers = {
-        "X-ApiKeys": f"accessKey={api_key}",
-        "Content-Type": "application/json"
-    }
+	headers = {
+		"X-ApiKeys": f"accessKey={api_key}",
+		"Content-Type": "application/json"
+	}
 
-    scan_data = {
-        "uuid": "your-uuid-here",
-        "settings": 
-            {
-            "name": "WebAppScan_oslo_kommune",
-            "enabled": true,
-            "text_targets": "oslo.kommune.no",
-            "launch": "ON_DEMAND",
-            "description": "Web application scan for oslo.kommune.no"
-          }
-    }
+	scan_data = {
+		"uuid": "your-uuid-here",
+		"settings": 
+			{
+			"name": "WebAppScan_oslo_kommune",
+			"enabled": true,
+			"text_targets": "oslo.kommune.no",
+			"launch": "ON_DEMAND",
+			"description": "Web application scan for oslo.kommune.no"
+		  }
+	}
 
-    response = requests.post(url, json=scan_data, headers=headers)
+	response = requests.post(url, json=scan_data, headers=headers)
 
-    if response.status_code == 200:
-        print("Scan created successfully!")
-    else:
-        print(f"Failed to create scan. Response: {response.text}")
+	if response.status_code == 200:
+		print("Scan created successfully!")
+	else:
+		print(f"Failed to create scan. Response: {response.text}")
 
 
 def InsertOKDomain(request):
-    blacklist = ['powerapps']
-    if request.method == 'POST':
-        form = OKDomainsForm(request.POST)
-        if form.is_valid():
-            # Extract form data and create a new OKDomains object
-            domain = form.cleaned_data['domain']
+	blacklist = ['powerapps']
+	if request.method == 'POST':
+		form = OKDomainsForm(request.POST)
+		if form.is_valid():
+			# Extract form data and create a new OKDomains object
+			domain = form.cleaned_data['domain']
 
-            # Check if domain is in blacklist 
-            for entry in blacklist:
-                if entry in str(domain):
-                    context = {'uploadmessage': 'Error: Domain is blacklisted'}
-                    return render(request, 'domains_upload.html', context)
+			# Check if domain is in blacklist 
+			for entry in blacklist:
+				if entry in str(domain):
+					context = {'uploadmessage': 'Error: Domain is blacklisted'}
+					return render(request, 'domains_upload.html', context)
 
-            okdomains, created = OKDomains.objects.get_or_create(domain=domain)
+			okdomains, created = OKDomains.objects.get_or_create(domain=domain)
 
-            if created:
-                # New domain, set all fields from form data
-                okdomains.registrar = form.cleaned_data['registrar']
-                okdomains.server = form.cleaned_data['server']
-                okdomains.system = form.cleaned_data['system']
-                okdomains.system_owner = form.cleaned_data['system_owner']
-                okdomains.comments = form.cleaned_data['comments']
-                okdomains.changes_since_last = "Initial upload"
-            else:
-                # Existing domain, update fields only if not empty
-                if form.cleaned_data['registrar']:
-                    okdomains.registrar = form.cleaned_data['registrar']
-                if form.cleaned_data['server']:
-                    okdomains.server = form.cleaned_data['server']
-                if form.cleaned_data['system']:
-                    okdomains.system = form.cleaned_data['system']
-                if form.cleaned_data['system_owner']:
-                    okdomains.system_owner = form.cleaned_data['system_owner']
-                if form.cleaned_data['comments']:
-                    okdomains.comments = form.cleaned_data['comments']
-                okdomains.changes_since_last = "Updated from upload"
+			if created:
+				# New domain, set all fields from form data
+				okdomains.registrar = form.cleaned_data['registrar']
+				okdomains.server = form.cleaned_data['server']
+				okdomains.system = form.cleaned_data['system']
+				okdomains.system_owner = form.cleaned_data['system_owner']
+				okdomains.comments = form.cleaned_data['comments']
+				okdomains.changes_since_last = "Initial upload"
+			else:
+				# Existing domain, update fields only if not empty
+				if form.cleaned_data['registrar']:
+					okdomains.registrar = form.cleaned_data['registrar']
+				if form.cleaned_data['server']:
+					okdomains.server = form.cleaned_data['server']
+				if form.cleaned_data['system']:
+					okdomains.system = form.cleaned_data['system']
+				if form.cleaned_data['system_owner']:
+					okdomains.system_owner = form.cleaned_data['system_owner']
+				if form.cleaned_data['comments']:
+					okdomains.comments = form.cleaned_data['comments']
+				okdomains.changes_since_last = "Updated from upload"
 
-            okdomains.save()
-            context = {'uploadmessage': 'Upload completed'}
-            return render(request, 'domains_upload.html', context)
+			okdomains.save()
+			context = {'uploadmessage': 'Upload completed'}
+			return render(request, 'domains_upload.html', context)
 
-        elif request.method == 'POST' and request.FILES:
-            uploaded_file = request.FILES['file']  # Assuming you have a file input field named 'file' in your form
+		elif request.method == 'POST' and request.FILES:
+			uploaded_file = request.FILES['file']  # Assuming you have a file input field named 'file' in your form
 
-            # Decode the uploaded file and split it into lines
-            file_content = uploaded_file.read().decode('utf-8')
-            domain_names = file_content.strip().split('\n')
+			# Decode the uploaded file and split it into lines
+			file_content = uploaded_file.read().decode('utf-8')
+			domain_names = file_content.strip().split('\n')
 
-            for domain_name in domain_names:
-                domain_name = domain_name.strip().replace("www.", "")
+			for domain_name in domain_names:
+				domain_name = domain_name.strip().replace("www.", "")
 
-                # Check if the domain already exists in the database
-                if not OKDomains.objects.filter(domain=domain_name).exists():
-                    # Create a new OKDomains object if the domain is not present
-                    ok_domain = OKDomains(domain=domain_name)
-                    ok_domain.save()
+				# Check if the domain already exists in the database
+				if not OKDomains.objects.filter(domain=domain_name).exists():
+					# Create a new OKDomains object if the domain is not present
+					ok_domain = OKDomains(domain=domain_name)
+					ok_domain.save()
 
-    else:
-        form = OKDomainsForm()
-        context = {'form': form}
-        return render(request, 'domains_upload.html', context)
+	else:
+		form = OKDomainsForm()
+		context = {'form': form}
+		return render(request, 'domains_upload.html', context)
