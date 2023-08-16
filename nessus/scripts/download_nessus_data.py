341296bf1f2148d2701dd18ea4f1afbdefb9a02f
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-08-16 12:31:49 +0200

    Update download_nessus_data.py

--- nessus/scripts/download_nessus_data.py
+++ nessus/scripts/download_nessus_data.py
@@ -31,7 +31,6 @@ def download_exported_scan():
 
         response_data = response.json()
         download_export_id = response_data["file"]
-        print(download_export_id)
 
         download_url = f"{url}/scans/{scan_id}/export/{download_export_id}/download"
         download_response = requests.get(download_url, headers=headers, verify=False)
