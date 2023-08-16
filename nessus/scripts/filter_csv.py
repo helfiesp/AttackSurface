aa6d106fedc3cc5da0768ca3b93bc65cd49f677c
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-08-16 08:55:55 +0200

    Update filter_csv.py

--- nessus/scripts/filter_csv.py
+++ nessus/scripts/filter_csv.py
@@ -1,11 +1,25 @@
-def iterate_csv_file(filename):
+import csv
+import json
+
+def convert_csv_to_json(filename):
     try:
+        json_objects = []
         with open(filename, "r") as file:
-            for line in file:
-                print(line.strip())  # Strip whitespace and print each line
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                json_objects.append(row)
+        return json_objects
     except FileNotFoundError:
         print("File not found.")
 
 if __name__ == "__main__":
     exported_file = "data/exported_scan_20.csv"  # Update with the actual filename
-    iterate_csv_file(exported_file)
+    json_objects = convert_csv_to_json(exported_file)
+
+    for obj in json_objects:
+        print(json.dumps(obj, indent=4))  # Print each JSON object with indentation
+
+
+
+
+
