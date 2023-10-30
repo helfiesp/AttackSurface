18fe6de20c96bd5ac6d5d032635464a83d3c4c1b
helfiesp <59867398+helfiesp@users.noreply.github.com>
2023-10-30 08:04:52 +0100

    Changes to nessus

--- nessus/scripts/download_nessus_data.py
+++ nessus/scripts/download_nessus_data.py
@@ -5,59 +5,61 @@ from requests.exceptions import RequestException
 import sqlite3
 import sys
 import time
+from misc import secrets
 
 # Append the path to the 'misc' directory to sys.path
 sys.path.append("/var/csirt/source/scanner")
 
-# Now you can import the entire 'secrets' module
-from misc import secrets
+
+
+scan_ids = [20]
 
 def download_exported_scan():
     try:
-        scan_id = 20  # Update this with the actual scan ID
-        url = "https://nessus.okcsirt.no"
-        access_key = os.environ["NESSUS_API_ACCESS_KEY"]
-        secret_key = os.environ["NESSUS_API_SECRET_KEY"]
+        for scan_id in scan_ids:
+            url = "https://nessus.okcsirt.no"
+            access_key = os.environ["NESSUS_API_ACCESS_KEY"]
+            secret_key = os.environ["NESSUS_API_SECRET_KEY"]
 
-        headers = {
-            "X-ApiKeys": f"accessKey={access_key};secretKey={secret_key}",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+            headers = {
+                "X-ApiKeys": f"accessKey={access_key};secretKey={secret_key}",
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
 
-        export_format_value = "csv"
+            export_format_value = "csv"
 
-        export_url = f"{url}/scans/{scan_id}/export"
-        payload = {"format": export_format_value}
+            export_url = f"{url}/scans/{scan_id}/export"
+            payload = {"format": export_format_value}
 
-        response = requests.post(export_url, headers=headers, data=payload, verify=False)
-        response.raise_for_status()
+            response = requests.post(export_url, headers=headers, data=payload, verify=False)
+            response.raise_for_status()
 
-        response_data = response.json()
-        download_export_id = response_data["file"]
-        time.sleep(30)
-        download_url = f"{url}/scans/{scan_id}/export/{download_export_id}/download"
-        download_response = requests.get(download_url, headers=headers, verify=False)
-        download_response.raise_for_status()
+            response_data = response.json()
+            download_export_id = response_data["file"]
+            time.sleep(30)
+            download_url = f"{url}/scans/{scan_id}/export/{download_export_id}/download"
+            download_response = requests.get(download_url, headers=headers, verify=False)
+            download_response.raise_for_status()
 
-        # Convert the downloaded content to a string
-        exported_scan_data = download_response.content.decode("utf-8")
+            # Convert the downloaded content to a string
+            exported_scan_data = download_response.content.decode("utf-8")
 
-        # Connect to the SQLite database
-        db_path = "/var/csirt/source/scanner/db.sqlite3"
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
+            # Connect to the SQLite database
+            db_path = "/var/csirt/source/scanner/db.sqlite3"
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
 
-        # Get the current date and time
-        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # Get the current date and time
+            current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # Insert exported scan data into the 'nessus_nessusdata' table
-        insert_query = "INSERT INTO nessus_nessusdata (data, date, scan_id) VALUES (?, ?, ?);"
-        cursor.execute(insert_query, (exported_scan_data, current_date, scan_id))
-        conn.commit()
+            # Insert exported scan data into the 'nessus_nessusdata' table
+            insert_query = "INSERT INTO nessus_nessusdata (data, date, scan_id) VALUES (?, ?, ?);"
+            cursor.execute(insert_query, (exported_scan_data, current_date, scan_id))
+            conn.commit()
 
-        conn.close()
+            conn.close()
 
-        print("Exported scan data saved to the database.")
+            print("Exported scan data saved to the database.")
     except RequestException as e:
         print(f"Error: {e}")
 
---
+++ nessus/scripts/fetch_nessus_scanids.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from misc import secrets
+
+def get_nessus_scan_ids():
+    # API endpoint to fetch the list of scans
+    endpoint = "/scans"
+    
+    # Connection details
+    url = "https://nessus.okcsirt.no" + endpoint
+    access_key = os.environ["NESSUS_API_ACCESS_KEY"]
+    secret_key = os.environ["NESSUS_API_SECRET_KEY"]
+
+    headers = {
+        "X-ApiKeys": f"accessKey={access_key};secretKey={secret_key}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        scans = response.json()["scans"]
+        scan_ids = [scan["id"] for scan in scans]
+        return scan_ids
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+if __name__ == "__main__":
+    scan_ids = get_nessus_scan_ids()
+    if scan_ids:
+        for id in scan_ids:
+            print(f"Scan ID: {id}")
+    else:
+        print("Failed to fetch scan IDs")
+
